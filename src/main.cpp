827132cfb4936aa3dbe16b1499c95e7109081022
Rene Allen <reneallen75@gmail.com>
2024-04-13 16:39:39 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+
+
+#include <opencv2/opencv.hpp>
+
+int main(){
+    //Testing if Open CV is installed
+    std::cout << CV_VERSION << std::endl;
+
+
+    
+  
+    return 0;
+}
