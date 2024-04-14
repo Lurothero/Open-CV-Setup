c7337a4e97e3ca2756e5b83becea9b3089d922ae
Rene Allen <reneallen75@gmail.com>
2024-04-13 18:32:13 -0600

    changed img and add vid

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,51 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-// Forward declearation
 
+//foward declear
+void printImageData(Mat &image);
 
 
 // Basic image processing
-int Run(){
+int Run()
+{
 
+    std::string img_path = "..\\..\\res\\Muffet_cycle_ServerIco_ clothed.png";
+    Mat img = imread(img_path, IMREAD_COLOR);
 
-    std::string img_path = "..\\..\\res\\girl.jpg";
-    Mat img = imread(img_path,IMREAD_COLOR);
+    //Dont on 2k pict with 12million numbers
+    //printImageData(img);
 
-    if(img.empty()){
-        std::cout << "Failed to read image... " <<img_path <<std::endl;
+    if (img.empty())
+    {
+        std::cout << "Failed to read image... " << img_path << std::endl;
         return 1;
     }
 
+    // Create window
+    namedWindow("Muffet",WINDOW_KEEPRATIO); // To pass the image to a window it MUST BE THE SAME NAME
+    imshow("Muffet", img);
 
-    //Create window
-    namedWindow("Girl",WINDOW_AUTOSIZE);//To pass the image to a window it MUST BE THE SAME NAME
-    imshow("Girl",img);
     waitKey(0);
-    destroyWindow("Girl");
-  
+    destroyWindow("Muffet");
+
     return 0;
 }
 
+void printImageData(Mat &image)
+{
+    std::cout << "Raw Image Data: " << std::endl << std::endl;
+    std::cout << format(image,Formatter::FMT_PYTHON) << std::endl;
+}
 
 int main()
 {
     // Testing if Open CV is installed
     std::cout << CV_VERSION << std::endl;
 
-    if(Run() == 1){
+    if (Run() == 1)
+    {
         return 1;
-
     }
     return 0;
 }
-
